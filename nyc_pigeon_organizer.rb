06dc7a247468d406a68184d53b52339fae66ef91
David Molina <davidmolina2810@gmail.com>
2020-08-24 20:23:55 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,22 +1,8 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
-  new_hash = {} #create empty hash to store altered hash
-  new_keys = []
-  new_inner_hash_keys = data.keys
-  new_inner_hash_values = []
-  data.each do |key, value|
-    new_inner_hash_values << data[key].keys
-    value.each do |key2, val2|
-      if !(new_keys.include?(val2))
-        new_keys << val2
-      end
-    end
+  data.each_with_object({}) do |key, value, new_hash|
+    binding.pry 
   end
-  new_keys.each do |name|
-    new_inner_hash_keys.each do |key|
-      new_inner_hash_values.each do |value|
-        new_hash = {
-          name => {
-            key => new_inner_hash_values[]
-          }
-        }
+end
+
