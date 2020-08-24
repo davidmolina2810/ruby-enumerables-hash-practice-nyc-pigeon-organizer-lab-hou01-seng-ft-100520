dcb60963228daf61c156793c6ff5a271d63f1dd4
David Molina <davidmolina2810@gmail.com>
2020-08-24 21:27:25 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -11,7 +11,7 @@ def nyc_pigeon_organizer(data)
         if !new_hash[name][key]
           new_hash[name][key] = []
         end
-        new_hash[name][key].push(inner_key)
+        new_hash[name][key].push(nested_key.to_s)
       end
     end
   end
