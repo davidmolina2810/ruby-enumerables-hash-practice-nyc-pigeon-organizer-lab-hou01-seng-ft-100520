16cc81869e6886190ea992eb639716ebe7358c71
David Molina <davidmolina2810@gmail.com>
2020-08-24 21:18:28 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,7 @@
 require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
-  data.each_with_object({}) do |(key, value), new_hash|
+  final_hash = data.each_with_object({}) do |(key, value), new_hash|
     #binding.pry
     value.each do |nested_key, names|
       #binding.pry
