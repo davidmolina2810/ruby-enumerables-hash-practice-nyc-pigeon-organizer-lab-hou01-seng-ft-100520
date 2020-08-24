521b6fb056e391177814b1b197aab3f1250360e2
David Molina <davidmolina2810@gmail.com>
2020-08-24 21:19:08 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -13,7 +13,6 @@ def nyc_pigeon_organizer(data)
           new_hash[name][key] = []
         end
         new_hash[name][key].push(inner_key.to_s)
-      new_hash[names] = nested_key
     end
   end
   binding.pry
