c43485284e1c283d340003998422f358561f3874
David Molina <davidmolina2810@gmail.com>
2020-08-24 21:30:13 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,6 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
   final_hash = data.each_with_object({}) do |(key, value), new_hash|
-    #binding.pry
     value.each do |nested_key, names|
       names.each do |name|
         if !new_hash[name]
